0fc972383e69ac0818166e92978d1a7bb19d1d41
YASHVARDHAN-DEV <80517622+YASHVARDHAN-DEV@users.noreply.github.com>
2021-07-22 21:38:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var background, backgroundImg;
+var snow, snowImg
+
+function preload(){
+  = loadImage("snow1.jpg");
+  = loadImage("snow2.jpg")
+}
+
+function setup() {
+  createCanvas(800,400);
+  //createSprite(400, 200, 50, 50);
+
+  
+
+
+function draw() {
+  //background(255,255,255);  
+  drawSprites();
+}
